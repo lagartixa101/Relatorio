f15b561548323c15a2b1efa2e1c07d6aad8a837e
Thiago CH. B. Silva <106824245+lagartixa101@users.noreply.github.com>
2023-10-02 10:29:05 -0300

    Update main.js

--- Projeto-3-Trimestre-main/main.js
+++ Projeto-3-Trimestre-main/main.js
@@ -1,1 +1,23 @@
-alert("Olá, Mundo!");
+function TocarSomPom(){
+    document.querySelector("#som_tecla_pom").play();
+}
+
+document.querySelector('.tecla_pom').onClick = TocarSomPom;
+
+function TocarSomA(){
+    document.querySelector("#som_tecla_A").play();
+}
+
+document.querySelector('.botao_novo_1').onClick = TocarSomA;
+
+function TocarSomOof(){
+    document.querySelector("#som_tecla_oof").play();
+}
+
+document.querySelector('.botao_novo_2').onClick = TocarSomOof;
+
+function TocarSomPipe(){
+    document.querySelector("#som_tecla_Pipe").play();
+}
+
+document.querySelector('.botao_novo_3').onClick = TocarSomPipe;
