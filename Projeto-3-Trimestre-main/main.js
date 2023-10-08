306067f4100b9bfbbcb9d5f34e2108270fdbdc0a
Thiago CH. B. Silva <106824245+lagartixa101@users.noreply.github.com>
2023-10-08 00:40:03 -0300

    Update main.js

--- Projeto-3-Trimestre-main/main.js
+++ Projeto-3-Trimestre-main/main.js
@@ -2,22 +2,22 @@ function TocarSomPom(){
     document.qwerySelector("#som_tecla_pom").play();
 }
 
-document.qwerySelector('.tecla_pom').onClick = TocarSomPom;
+document.qwerySelector('.tecla_pom').onClick = TocarSomPom();
 
 function TocarSomA(){
     document.qwerySelector("#som_tecla_A").play();
 }
 
-document.qwerySelector('.botao_novo_1').onClick = TocarSomA;
+document.qwerySelector('.botao_novo_1').onClick = TocarSomA();
 
 function TocarSomOof(){
     document.qwerySelector("#som_tecla_oof").play();
 }
 
-document.qwerySelector('.botao_novo_2').onClick = TocarSomOof;
+document.qwerySelector('.botao_novo_2').onClick = TocarSomOof();
 
 function TocarSomPipe(){
     document.qwerySelector("#som_tecla_Pipe").play();
 }
 
-document.qwerySelector('.botao_novo_3').onClick = TocarSomPipe;
+document.qwerySelector('.botao_novo_3').onClick = TocarSomPipe();
