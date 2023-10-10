4914cab6c9840f5ee15e98be01685a9918d03ffb
Thiago CH. B. Silva <106824245+lagartixa101@users.noreply.github.com>
2023-10-10 19:33:49 -0300

    Add files via upload

---
+++ Relatorio-main/Projeto-3-Trimestre-main/main.js
@@ -0,0 +1,55 @@
+function TocarSomPom(){
+    document.querySelector("#som_tecla_pom").play();
+}
+
+function TocarSomA(){
+    document.querySelector("#som_tecla_A").play();
+}
+
+function TocarSomOof(){
+    document.querySelector("#som_tecla_oof").play();
+}
+
+function TocarSomPipe(){
+    document.querySelector("#som_tecla_Pipe").play();
+}
+
+function TocarSomTim(){
+    document.querySelector("#som_tecla_tim").play();
+}
+
+function TocarSomPsh(){
+    document.querySelector("#som_tecla_psh").play();
+}
+
+function TocarSomPuff(){
+    document.querySelector("#som_tecla_puff").play();
+}
+
+function TocarSomClap(){
+    document.querySelector("#som_tecla_clap").play();
+}
+
+function TocarSomToim(){
+    document.querySelector("#som_tecla_toim").play();
+}
+
+function TocarSomSplash(){
+    document.querySelector("#som_tecla_splash").play();
+}
+
+function TocarSomTic(){
+    document.querySelector("#som_tecla_tic").play();
+}
+
+function TocarSomTom(){
+    document.querySelector("#som_tecla_tom").play();
+}
+
+function TocarSomPare(){
+    document.querySelector("#som_tecla_pare").play();
+}
+
+function TocarTudo(){
+    document.querySelectorAll("audio");
+}
